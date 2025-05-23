e6c7e6e6e33aa252d79383bb8a9749943d9d47ef
noiioiu <noiioiu@cocaine.ninja>
2025-05-22 20:28:24 -0500

    python3Packages.snappy: init at 3.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16347,6 +16347,8 @@ self: super: with self; {
 
   snapcast = callPackage ../development/python-modules/snapcast { };
 
+  snappy = callPackage ../development/python-modules/snappy { };
+
   snappy-15-knots = callPackage ../development/python-modules/snappy-15-knots { };
 
   snappy-manifolds = callPackage ../development/python-modules/snappy-manifolds { };
